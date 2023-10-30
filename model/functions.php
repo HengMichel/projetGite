71071c04f634bf385fe02dad1f8a5b80cadddce2
cavacmoi <cavacmoi@gmail.com>
2023-10-30 21:41:16 +0100

    cleanCode

--- model/functions.php
+++ model/functions.php
@@ -2,23 +2,6 @@
 require_once $_SERVER["DOCUMENT_ROOT"]."/projetGite/inc/database.php";
 // require_once "./inc/database.php";
 
-function  hotelList(){
-    //  se connecter a la db (database) ou bd (base de données)
-    $db = dbConnexion();
-    // preparer la requete
-    $request = $db->prepare("SELECT * FROM hotels");
-    // executer la requete
-    $listHotel = null;
-    try {
-        $request->execute();
-        // récuperer le resultat dans un tableau
-        $listHotel = $request->fetchAll(PDO::FETCH_ASSOC);
-    } catch ( PDOException $e) {
-        echo $e->getMessage();
-    }
-    return $listHotel;
-}
-
 function  roomList(){
     //  se connecter a la db (database) ou bd (base de données)
     $db = dbConnexion();

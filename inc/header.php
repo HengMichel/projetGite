63242d92ffbde4a5fa6461b9b450e2bdfc5c346e
cavacmoi <cavacmoi@gmail.com>
2023-11-01 17:01:53 +0100

    kickMediaQuery

--- inc/header.php
+++ inc/header.php
@@ -17,9 +17,7 @@
     <div class="container-fluid ">
       <!-- <a class="navbar-brand table-group-divider fw-bold" href="https://autumn-drunk.000webhostapp.com "> -->
       <a class="navbar-brand fw-bold link-warning" href="http://localhost/projetGite/index.php">Le Gîte DAKOTÉ</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+     
 
       <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarText">
         <ul class="navbar-nav  mb-2 mb-lg-0">

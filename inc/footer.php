e7b94b5fd2b41335231d188ae971dc20b2d36a79
cavacmoi <cavacmoi@gmail.com>
2023-11-07 21:21:07 +0100

    addFooter

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,10 @@
 
-    <!--ici creer un footer  -->
+<div class="pied d-felx fa-2x text-sm-center">
+    <i class="fa-brands fa-square-snapchat fa-spin fa-spin-reverse link-info"></i>
+    <i class="fa-brands fa-facebook fa-beat-fade link-success"></i>
+    <i class="fa-brands fa-tiktok fa-shake link-warning"></i>
+    <i class="fa-brands fa-twitter fa-beat-fade link-primary"></i>
+    <i class="fa-brands fa-square-instagram fa-spin link-danger"></i></div>
+</div>
 </body>
 </html>

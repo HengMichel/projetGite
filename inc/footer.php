e9e448fe01cc175186bff86d2eabeb1048a71ab8
cavacmoi <cavacmoi@gmail.com>
2023-11-08 20:04:22 +0100

    addFooter

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,10 @@
 
 <div class="pied d-felx fa-2x text-sm-center">
-    <i class="fa-brands fa-square-snapchat fa-spin fa-spin-reverse link-info"></i>
-    <i class="fa-brands fa-facebook fa-beat-fade link-success"></i>
-    <i class="fa-brands fa-tiktok fa-shake link-warning"></i>
+    <a href="https://www.linkedin.com/in/michel-h-47b3641b0"><i class="fa-brands fa-linkedin fa-bounce link-warning"></i></a>
+    <a href="https://github.com/HengMichel"><i class="fa-brands fa-github fa-flip link-light"></i></a>
+    <i class="fa-brands fa-square-snapchat fa-spin fa-spin-reverse link-success"></i>
+    <i class="fa-brands fa-facebook fa-spin-pulse link-dark"></i>
+    <i class="fa-brands fa-tiktok fa-shake link-info"></i>
     <i class="fa-brands fa-twitter fa-beat-fade link-primary"></i>
     <i class="fa-brands fa-square-instagram fa-spin link-danger"></i></div>
 </div>

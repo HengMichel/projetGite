9ba595b9c59694745e9ae69dc1f837d2cbad704c
heng michel <cavacmoi@gmail.com>
2023-11-09 16:53:14 +0100

    mediaQueryUpdate+2pages

--- aboutUs.php
+++ aboutUs.php
@@ -4,16 +4,16 @@ include_once "inc/header.php";
 ?>
 <div class="container">
     <div class="bienvenue link-warning">
-
         <h1>Bienvenue au gîte DAKOTÉ</h1>
     </div>
     <div class="descriptionDescriptif border border-4 border-warning">
         <h3>Descriptif</h3>
-        <p>Descriptif 2023, susceptible de modifications pour 2024, merci de vous référer à la fiche descriptive validée lors de la réservation.
+        <p class="p1">Descriptif 2023, susceptible de modifications pour 2024, merci de vous référer à la fiche descriptive validée lors de la réservation.
 
 Vous aimez la mer, les sports nautiques, les belles randonnées et la nature préservée? Vous êtes au bon endroit! Le petit port touristique et animé de l'Aber Wrac'h est également réputé pour son centre nautique, les bateaux, ses magnifiques panoramas sur l'Aber et le phare de l'île Vierge, l'île Wrac'h, ... Les dunes Ste Marguerite sont également connues pour ses spots de kite-surf et de planche à voile, ses dunes sauvages, ses magnifiques randonnées, ses plages de sable blanc et eaux turquoises au pays des huîtres. Et c'est aussi le point de départ idéal pour visiter le Nord Finistère,
 
 Au Port de l'Aber Wrac'h, cette belle maison neuve en bois est confortable et lumineuse a été aménagée en 2 gîtes mitoyens avec terrasses closes privatives et d'un jardin bordé d'un petit ruisseau rafraîchissant. Au rez-de-chaussée, elle comprend une entrée, une cuisine ouverte sur le séjour salon lumineux avec ses grandes baies vitrées et accès à la terrasse bois, un wc. A l'étage, une grande chambre avec 1lit 2 personnes 160x200 et un lit banquette 90x190 (poss pers supp), une salle de bain avec baignoire équipée d'une douche et un meuble vasque. A disposition également une buanderie avec lave linge, sèche linge et abri pour vos vélos.</p>
+        <p class="p2bis">Vous aimez la mer, les sports nautiques, les belles randonnées et la nature préservée? Vous êtes au bon endroit! Le petit port touristique et animé de l'Aber Wrac'h est également réputé pour son centre nautique, les bateaux, ses magnifiques panoramas sur l'Aber et le phare de l'île Vierge, l'île Wrac'h, ... Les dunes Ste Marguerite sont également connues pour ses spots de kite-surf et de planche à voile, ses dunes sauvages, ses magnifiques randonnées, ses plages de sable blanc et eaux turquoises au pays des huîtres.</p>
     </div>
     <div class="proprio">
        <h4>Les propriétaires</h4>
@@ -22,12 +22,10 @@ Au Port de l'Aber Wrac'h, cette belle maison neuve en bois est confortable et lu
            <img class="pro border border-4 border-success" src="assets/imgs/les_proprio.jpg " alt="">
        </div>
     </div>
-
     <div class="local mt-3 link-warning">
         <h3>Localisation</h3>
-        <iframe class="map mt-3 border border-4 border-warning" width="100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2628.7214751326624!2d2.3966389!3d48.7872074!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6739a5f64b489%3A0x5fa105b2ca801da8!2sCentre%20des%20Finances%20Publiques!5e0!3m2!1sfr!2sfr!4v1698741539012!5m2!1sfr!2sfr" width="1200" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-
+    <iframe class="map mt-3 border border-4 border-warning" width="100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2628.7214751326624!2d2.3966389!3d48.7872074!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6739a5f64b489%3A0x5fa105b2ca801da8!2sCentre%20des%20Finances%20Publiques!5e0!3m2!1sfr!2sfr!4v1698741539012!5m2!1sfr!2sfr" width="1200" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 <?php
 include_once "inc/footer.php";

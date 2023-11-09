038b5201184195ef6512fb9bf93ab93964f87adb
cavacmoi <cavacmoi@gmail.com>
2023-11-09 21:27:15 +0100

    clearMediaQuery

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 
-<!-- <div class="pied d-felx fa-2x text-sm-center">
+<div class="pied d-felx fa-2x text-sm-center">
     <a href="https://www.linkedin.com/in/michel-h-47b3641b0"><i class="fa-brands fa-linkedin fa-bounce link-warning"></i></a>
     <a href="https://github.com/HengMichel"><i class="fa-brands fa-github fa-flip link-light"></i></a>
     <i class="fa-brands fa-square-snapchat fa-spin fa-spin-reverse link-success"></i>
@@ -7,6 +7,6 @@
     <i class="fa-brands fa-tiktok fa-shake link-info"></i>
     <i class="fa-brands fa-twitter fa-beat-fade link-primary"></i>
     <i class="fa-brands fa-square-instagram fa-spin link-danger"></i></div>
-</div> -->
+</div>
 </body>
 </html>

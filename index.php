7f5913a5fd4a404945a2c397cca86b0c2f05dba5
heng michel <cavacmoi@gmail.com>
2023-12-07 11:07:14 +0100

    needCheckMediaQuerry480px

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once "./model/functions.php";
 $listRoom = roomList();
 ?>
 
-<div class="container d-flex flex-wrap justify-content-around">
+<div class="container2">
     <?php foreach($listRoom as $room){ ?>
         <!-- fa-beat-fade -->
-        <div class="card border-warning border-4 mb-5 " style="width: 18rem;">
+        <div class="card border-warning border-4 mb-5" style="width: 20rem;">
             <div class="img_room">
                 <img src="assets/imgs/chambres/<?= $room["room_imgs"] ?> " class="card-img-top img-fluid" alt="image">
             </div>
@@ -16,7 +16,7 @@ $listRoom = roomList();
                 <p class="card-text fa-2x fw-medium"><?= $room["price"]; ?>€/nuit</p>
                 <p class="card-text link-warning fa-xl"><?= $room["category"]; ?></p>
                 <p class="card-text fw-medium"><?= $room["persons"]; ?> Persons</p>
-                <a class="btn bg-success border-warning border-4 link-warning m-0" 
+                <a class="btn bg-warning fw-bolder border-light border-2 link-success m-0" 
                 href="./booking.php?room=
                 <?= $room["id_room"]; ?>&price=<?= $room['price'] ?>">Book this Room</a>
             </div>
